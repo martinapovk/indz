189d43f447fec9587a161a4cc1cfd7023a6276a3
martinapovk <martinacskapok@gmail.com>
2022-11-08 01:09:40 +0200

    update

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  // $(".start-1").owlCarousel({
+  //   items: 1,
+  //   nav: true,
+  //   navText: [
+  //     '<i class="fas fa-angle-left"></i>',
+  //     '<i class="fas fa-angle-right"></i>',
+  //   ],
+  // });
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    nav: true,
+    navText: [
+      '<i class="fas fa-chevron-left"></i>',
+      '<i class="fas fa-chevron-right"></i>',
+    ],
+  });
+});
